9db03830a49b5defd02ff241c5e0bcfe4e07132e
Futurama816 <davit.mdzinarishvili.1@iliauni.edu.ge>
2025-03-02 20:11:17 +0400

    feat: Stack, Queue and remove
    added Stack, Queue and Remove functions

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.entries = [];
+  }
+  push(element) {
+    this.entries.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.entries.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.entries[this.entries.length -1];
   }
 }
 
